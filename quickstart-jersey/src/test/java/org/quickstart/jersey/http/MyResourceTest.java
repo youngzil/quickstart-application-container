ebee00b758b06d13a96f388621d952e2181cc075
yangzl <youngzil@163.com>
2019-07-20 15:44:16 +0800

    修改包名

---
+++ quickstart-jersey/src/test/java/org/quickstart/jersey/http/MyResourceTest.java
@@ -0,0 +1,27 @@
+package org.quickstart.jersey.http;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+public class MyResourceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(HelloWorldResource.class);
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        final String responseMsg = target().path("helloworld").request().get(String.class);
+
+        assertEquals(HelloWorldResource.CLICHED_MESSAGE, responseMsg);
+    }
+}
---
+++ quickstart-tomcat/src/main/java/org/quickstart/tomcat/TestBoot.java
@@ -0,0 +1,22 @@
+/**
+ * 项目名称：quickstart-container-tomcat 
+ * 文件名：TestBoot.java
+ * 版本信息：
+ * 日期：2018年4月25日
+ * Copyright yangzl Corporation 2018
+ * 版权所有 *
+ */
+package org.quickstart.tomcat;
+
+/**
+ * TestBoot 
+ *  
+ * @author：youngzil@163.com
+ * @2018年4月25日 下午12:41:07 
+ * @since 1.0
+ */
+public class TestBoot {
+    public static void main(String[] args) {
+        WebServce.getWebserce().start();
+    }
+}
